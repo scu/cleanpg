3b74c9d07c6512e0b7127e1d6250291d6191ad72
Scott Underwood <sunderwood@gmail.com>
2020-11-22 14:04:34 -0800

    Switch from the Go core flag package to github.com/scu/flagplus

--- cleanpg.go
+++ cleanpg.go
@@ -6,56 +6,36 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/scu/cleanpg/cleanhtml"
 	"github.com/scu/cleanpg/logger"
+	"github.com/scu/flagplus"
 )
-
-// Command line flags usage map
-// So usage descriptions can be shared across long and short options
-var usageMap = map[string]string{
-	"help":    "Help",
-	"verbose": "Print extra debugging information",
-	"output":  "HTML file to render to [default=stdout]",
-	"save":    "Save a copy of the source HTML document",
-	"posth1":  "Render body elements after first h1 tag",
-	"nostyle": "Do not automatically render tag-level embedded styles",
-	"nolinks": "Do not render links",
-}
-
-// Command line flags
-var (
-	helpPtr    = flag.Bool("help", false, usageMap["help"])
-	verbosePtr = flag.Bool("verbose", false, usageMap["verbose"])
-	outputPtr  = flag.String("output", "", usageMap["output"])
-	savePtr    = flag.String("save", "", usageMap["save"])
-	posth1Ptr  = flag.Bool("posth1", false, usageMap["posth1"])
-	noStylePtr = flag.Bool("nostyle", false, usageMap["nostyle"])
-	noLinksPtr = flag.Bool("nolinks", false, usageMap["nolinks"])
-)
-
-// Holds the fd for the output [default=stdout]
-var outFile *os.File = os.Stdout
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: cleanpg [flags] url\n")
-	flag.PrintDefaults()
+	fmt.Println(fs.Usage())
 }
 
 func init() {
-	// Add optional short arguments to flag map
-	flag.BoolVar(helpPtr, "?", false, usageMap["help"])
-	flag.BoolVar(verbosePtr, "v", false, usageMap["verbose"])
-	flag.StringVar(outputPtr, "o", "", usageMap["output"])
-	flag.StringVar(savePtr, "s", "", usageMap["save"])
-	flag.BoolVar(posth1Ptr, "p", false, usageMap["posth1"])
-	flag.BoolVar(noStylePtr, "n", false, usageMap["nostyle"])
-	flag.BoolVar(noLinksPtr, "l", false, usageMap["nolinks"])
+	fs = flagplus.NewFlagSet("cleanpg")
+	fs.FlagSetDescription("Utility for rendering text-readable versions of HTML pages.")
+
+	// Add flags
+	fs.AddFlag("verbose", "v", "Print extra debugging information to stderr")
+	fs.AddFlag("help", "h", "Help")
+	fs.AddFlag("nocanon", "c", "Do not attempt to render canonically")
+	fs.AddFlag("nostyle", "n", "Do not render embedded style")
+	fs.AddFlag("nolinks", "l", "Do not render links")
+	fs.AddStringFlag("output", "o", "Write output to `file.html`", "out.html")
+	fs.AddStringFlag("save", "s", "Save source document as `file.html`", "")
+
+	fs.Parse()
 }
+
+var fs *flagplus.FlagSet
 
 func main() {
 	// Call cleanpgMain in a separate function
@@ -66,46 +46,58 @@ func main() {
 
 func cleanpgMain() int {
 
+	var outFile *os.File
+
 	// Set up logging
 	logger.Truncate()
 
-	// Flags
-	flag.Usage = usage
-	flag.Parse()
-
-	// Print help and exit
-	if *helpPtr {
+	// FLAG "help"
+	help, err := fs.Get("help")
+	if err != nil {
+		panic(err)
+	}
+	if help {
 		usage()
 		return 0
 	}
 
-	// Get url from argument
-	urlToClean := flag.Arg(0)
-	if urlToClean == "" {
-		fmt.Fprintf(os.Stderr, "Missing URL\n")
-		usage()
-		return 1
+	// FLAG "verbose"
+	printVerbose, err := fs.Get("verbose")
+	if err != nil {
+		panic(err)
 	}
-
-	// Optional flag: print extra data to stderr
-	if *verbosePtr {
+	if printVerbose {
 		logger.LogToStderr(true)
 	}
 
-	if *outputPtr != "" {
+	// FLAG "output"
+	outputFile, err := fs.GetString("output")
+	if err != nil {
+		panic(err)
+	}
+	if outputFile != "" {
 		// Verify is .html extension
-		if filepath.Ext(*outputPtr) != ".html" {
-			logger.Write(logger.FATAL, "file [%s] must have .html extension", *outputPtr)
+		if filepath.Ext(outputFile) != ".html" {
+			logger.Write(logger.FATAL, "file [%s] must have .html extension", outputFile)
 			return 1
 		}
 
 		var err error
 		// Create & open the file
-		outFile, err = os.Create(*outputPtr)
+		outFile, err = os.Create(outputFile)
 		if err != nil {
-			logger.Write(logger.FATAL, "could not open [%s]: %s", *outputPtr, err)
+			logger.Write(logger.FATAL, "could not open [%s]: %s", outputFile, err)
 			return 1
 		}
+	}
+
+	// Get url from argument
+	args := fs.GetArgs()
+	urlToClean := args[0]
+	if urlToClean == "" {
+		fmt.Fprintf(os.Stderr, "Missing URL\n")
+		usage()
+		return 1
 	}
 
 	logger.Write(logger.INFO, "reading data from URL=%s", urlToClean)
@@ -116,39 +108,55 @@ func cleanpgMain() int {
 		return 1
 	}
 
+	// FLAG "save"
+	saveFile, err := fs.GetString("save")
+	if err != nil {
+		panic(err)
+	}
 	// Save a copy in source.html
-	if *savePtr != "" {
+	if saveFile != "" {
 		// Verify is .html extension
-		if filepath.Ext(*savePtr) != ".html" {
-			logger.Write(logger.FATAL, "file [%s] must have .html extension", *savePtr)
+		if filepath.Ext(saveFile) != ".html" {
+			logger.Write(logger.FATAL, "file [%s] must have .html extension", saveFile)
 			return 1
 		}
-		svFile, err := os.Create(*savePtr)
+		svFile, err := os.Create(saveFile)
 		if err != nil {
-			logger.Write(logger.FATAL, "could not open save file [%s]: %s", *savePtr, err)
+			logger.Write(logger.FATAL, "could not open save file [%s]: %s", saveFile, err)
 			return 1
 		}
 		defer svFile.Close()
-		logger.Write(logger.INFO, "saving a copy of the source document to %s", *savePtr)
+		logger.Write(logger.INFO, "saving a copy of the source document to %s", saveFile)
 		fmt.Fprintf(svFile, "%s", sourceData)
 	}
 
-	// Optional flag: process body data only after h1
-	// flag is encountered (exception is <div> elements since
-	// <h1> may be encapsulated)
-	if *posth1Ptr {
+	// FLAG "nocanon"
+	nocanon, err := fs.Get("nocanon")
+	if err != nil {
+		panic(err)
+	}
+	if !nocanon {
+		// Canonical is default
 		cleanhtml.SetPostH1Render(true)
 		logger.Write(logger.INFO, "processing body elements after first <h1> tag")
 	}
 
-	// Optional flag: do not embed tag-level style.
-	if *noStylePtr {
+	// FLAG "nostyle"
+	noStyle, err := fs.Get("nostyle")
+	if err != nil {
+		panic(err)
+	}
+	if noStyle {
 		cleanhtml.SetStyleRender(false)
 		logger.Write(logger.INFO, "skipping automatic tag-level style embedding")
 	}
 
-	// Optional flag: do not render links.
-	if *noLinksPtr {
+	// FLAG "nolinks"
+	noLinks, err := fs.Get("nolinks")
+	if err != nil {
+		panic(err)
+	}
+	if noLinks {
 		cleanhtml.SetLinksRender(false)
 		logger.Write(logger.INFO, "not rendering links")
 	}
@@ -162,7 +170,8 @@ func cleanpgMain() int {
 
 	// Write to designated output
 	fmt.Fprintf(outFile, "%s", cleanData)
-	logger.Write(logger.INFO, "created a clean version of %s", urlToClean)
+	fmt.Printf("Document rendered to %q\n", outputFile)
+	logger.Write(logger.INFO, "Document from %q rendered to %q", urlToClean, outputFile)
 
 	return 0
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/scu/cleanpg
 
 go 1.15
 
-require golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+require (
+	github.com/scu/flagplus v1.0.0
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+)
